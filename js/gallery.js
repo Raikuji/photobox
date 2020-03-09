26be79a957e6639a2456265efda7f974ac2a8f84
Raikuji <irub.tech@gmail.com>
2020-03-09 14:42:23 +0100

    Ajout du filter pour ne recuperer qu'une photo

--- js/gallery.js
+++ js/gallery.js
@@ -14,10 +14,11 @@ export const load = ((url, uri) => {
         const json = JSON.parse(data);
         let photo;
         for(let i = 86; i < 90; i++) {
+            photo = json.filter(elem => elem.id == i)
             $('#photobox-gallery')
             .append('<div class="vignette" >')
-            .append('<img data-img=' + json.original.href + '\ndata-uri=' + json.links.self.href + '\nsrc=' + json.thumbnail.href)
-            .append('<div>' + json.titre + '</div></div>')
+            .append('<img data-img=' + photo.original.href + '\ndata-uri=' + photo.links.self.href + '\nsrc=' + photo.thumbnail.href)
+            .append('<div>' + photo.titre + '</div></div>')
             
         }
     });

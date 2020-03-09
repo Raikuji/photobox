590583e844762a1d4d20e9f12b79d0977f309ffd
lucasburte <lucas.burte@hotmail.fr>
2020-03-09 15:02:25 +0100

    fcghjk

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import * as gallery from './gallery.js';
 
 $(document).ready( function() {
-    $('#load_gallery').on('click', gallery.load('https://webetu.iutnc.univ-lorraine.fr', '/www/canals5/photobox/photos/?offset=8&size=12'))
+    $('#load_gallery').on('click', gallery.load('https://webetu.iutnc.univ-lorraine.fr', '/www/canals5/photobox/photos/?offset=8&size=12'),
+    console.log("here"));
 })

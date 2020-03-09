f108a2cd4efb150e4ab030cac6a93f9add9643b3
Raikuji <irub.tech@gmail.com>
2020-03-09 15:41:20 +0100

    Ajout du foreach

--- js/gallery.js
+++ js/gallery.js
@@ -10,19 +10,17 @@ export const init = ((id, offs) => {
 export const load = ((url, uri) => {
     photoload.init(url);
     const data = photoload.load(uri);
+    //console.log(data)
     const display = ((data) => {
-        const json = JSON.parse(data);
-        let photo;
-        console.log(json)
-        for(let i = 90; i < 100; i++) {
-            photo = json.filter(elem => elem.id == i)
+        //console.log(data)
+        data.forEach(photo => {
+            photo = data
             console.log(photo)
             $('#photobox-gallery')
             .append('<div class="vignette">')
             .append('<img data-img=' + photo.original.href + '\ndata-uri=' + photo.links.self.href + '\nsrc=' + photo.thumbnail.href)
             .append('<div>' + photo.titre + '</div></div>')
-            
-        }
+        });
     });
     display(data);
 })

1940c7c260653b7d6b067edef5665c6d35d84f59
Raikuji <irub.tech@gmail.com>
2020-03-09 14:36:46 +0100

    Ajout du parsing de json

--- js/gallery.js
+++ js/gallery.js
@@ -11,6 +11,14 @@ export const load = ((url, uri) => {
     photoload.init(url);
     const data = photoload.load(uri);
     ((data) => {
-        JSON.parse(data);
+        const json = JSON.parse(data);
+        let photo;
+        for(let i = 86; i < 90; i++) {
+            $('#photobox-gallery')
+            .append('<div class="vignette" >')
+            .append('<img data-img=' + json.original.href + '\ndata-uri=' + json.links.self.href + '\nsrc=' + json.thumbnail.href)
+            .append('<div>' + json.titre + '</div></div>')
+            
+        }
     });
 })

d0de77a6e9e012a29d179921f9291498a8558ab5
Raikuji <irub.tech@gmail.com>
2020-03-09 15:12:20 +0100

    Changement de methode de click

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import * as gallery from './gallery.js';
 
 $(document).ready( function() {
-    $('#load_gallery').on('click', gallery.load('https://webetu.iutnc.univ-lorraine.fr', '/www/canals5/photobox/photos/?offset=8&size=12'),
-    console.log("here"));
+    $('#load_gallery').click(() => gallery.load('https://webetu.iutnc.univ-lorraine.fr', '/www/canals5/photobox/photos/?offset=8&size=12'))
 })

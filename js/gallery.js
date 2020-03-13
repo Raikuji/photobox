9d2319ddbce075071dc6321a0c30c35bb36fb8ac
Raikuji <irub.tech@gmail.com>
2020-03-13 11:31:24 +0100

    Implementation API fini

--- js/gallery.js
+++ js/gallery.js
@@ -2,23 +2,25 @@ import * as photoload from './photoloader.js';
 
 let idGallery, offset;
 
+let servUrl;
+
 export const init = ((id, offs) => {
     idGallery = id;
     offset = offs;
 })
 
-export const load = ((url, uri) => {
-    photoload.init(url);
-    const data = photoload.load(uri);
-    console.log(data)
-    const display = ((data) => {
-        data.forEach(photo => {
-            photo = data
-            $('#photobox-gallery')
-            .append('<div class="vignette">')
-            .append('<img data-img=' + photo.original.href + '\ndata-uri=' + photo.links.self.href + '\nsrc=' + photo.thumbnail.href)
-            .append('<div>' + photo.titre + '</div></div>')
-        });
+const display = ((data) => {
+    data.data.photos.forEach(photo => {
+        //console.log(photo.photo)
+        $('#photobox-gallery')
+        .append('<div class="vignette"><img data-img=' + servUrl + photo.photo.original.href + '\ndata-uri=' + photo.links.self.href + '\nsrc=' + servUrl + photo.photo.thumbnail.href + '>' + photo.photo.titre + '</div></div>')
     });
-    display(data);
+});
+
+export const load = ((url, uri) => {
+    servUrl = url
+    photoload.init(url);
+    photoload.load(uri)
+    .then(display)
+    .catch((error) => (console.error(error))) 
 })
